8fd146716ce4e51948cd6d6b82b2892ee2051572
OMSAH <huncyoms@gmail.com>
2024-12-03 19:03:06 -0500

    added

---
+++ client/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./home.css"
+export default function Home() {
+  return (
+    <div className='home'>
+      <h4>Welcome etc...</h4>
+      <button className='btn'>Start</button>
+    </div>
+  )
+}
